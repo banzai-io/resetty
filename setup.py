efe565f3f8d7f8fd94a8609a7c4d2d7774e6eb16
Ignacio De La Madrid <ignacio.delamadrid@espressive.com>
2020-06-17 20:51:27 -0600

    Add setup cfg so that we can find the packages and run the migrations

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="resetty",
-    version="0.1",
-    packages=find_packages(exclude=["tests"]),
-    license="MIT",
-    description="Django Middleware that requires a user to reset password",
-    url="https://github.com/banzai-io/resetty/",
-    author="Ignacio De La Madrid",
-    author_email="ignacio@getbanzai.com",
-    install_requires=["django==2.2.11"],
-)
+setup()
