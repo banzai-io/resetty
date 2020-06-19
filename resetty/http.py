652f43f9c5d08cceba73257497900d9e6caf047e
Ignacio De La Madrid <ignacio.delamadrid@espressive.com>
2020-06-18 18:23:51 -0600

    Support 'next' query param and update django required version.

--- resetty/http.py
+++ resetty/http.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 
-
+# how do we support another token_generator?
 def account_reset_password_redirect_url(user, token_generator=default_token_generator):
     path_params = {
         "uidb64": urlsafe_base64_encode(force_bytes(user.pk)),
@@ -13,9 +13,9 @@ def account_reset_password_redirect_url(user, token_generator=default_token_gene
 
 
 def reset_password_redirect_url(user, path):
-
-    return (
+    redirect_to =  (
         reverse("admin:password_change")
         if path.startswith("/admin/")
         else account_reset_password_redirect_url(user)
     )
+    return f"{redirect_to}?next={path}"
