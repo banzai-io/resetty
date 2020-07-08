9c9cb18087605b73ea2c2f4e3c435d2b5b8472f1
Ignacio De La Madrid <ignacio.delamadrid@espressive.com>
2020-07-07 19:17:57 -0600

    set default reset passwor url to /admin/password_change

--- resetty/http.py
+++ resetty/http.py
@@ -1,21 +1,8 @@
-from django.contrib.auth.tokens import default_token_generator
 from django.urls import reverse
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
-
-# how do we support another token_generator?
-def account_reset_password_redirect_url(user, token_generator=default_token_generator):
-    path_params = {
-        "uidb64": urlsafe_base64_encode(force_bytes(user.pk)),
-        "token": token_generator.make_token(user),
-    }
-    return reverse("password_reset_confirm", kwargs=path_params)
+from .app_settings import RESET_PASSWORD_URL
 
 
 def reset_password_redirect_url(user, path):
-    redirect_to =  (
-        reverse("admin:password_change")
-        if path.startswith("/admin/")
-        else account_reset_password_redirect_url(user)
-    )
+    # default is /admin/password_change/
+    redirect_to = RESET_PASSWORD_URL or reverse("admin:password_change")
     return f"{redirect_to}?next={path}"
